ec9e24cb91ae971d7ea4257af0fb80e18802778e
Bernd <141633738+Bernd02@users.noreply.github.com>
2023-10-11 12:34:57 +0200

    Initial Commit

---
+++ MvcMovie/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// (B) Seed data on startup
+using (var scope = app.Services.CreateScope()) {
+	var services = scope.ServiceProvider;
+
+	// zie class Models.SeedData
+	SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// (B) Toegevoegd om client-side validation op decimal number fields te doen werken
+app.UseRequestLocalization("nl-be");
+
+app.Run();
